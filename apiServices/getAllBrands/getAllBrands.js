92158e9e7b0d60c7d84c1f496423d1c59f132e7c
valenn0101 <veroncaceresvalentin@gmail.com>
2023-04-30 13:08:52 -0300

    Status response corrected.

--- apiServices/getAllBrands/getAllBrands.js
+++ apiServices/getAllBrands/getAllBrands.js
@@ -1,9 +1,9 @@
-const prisma = require('../../prisma')
+const prisma = require('../../config/prisma')
 
 async function getAllBrands(req, res) {
   try {
     const brands = await prisma.brands.findMany()
-    res.json({ brands: [...brands], message: 'These are the brands' })
+    res.status(200).json({ brands: [...brands], message: 'These are the brands' })
   } catch (error) {
     res.status(500).json({ error: 'Failed to retrieve brands. Please try again later.' })
   } finally {
--- apiServices/getAllProducts/getAllProducts.js
+++ apiServices/getAllProducts/getAllProducts.js
@@ -1,9 +1,9 @@
-const prisma = require('../../prisma')
+const prisma = require('../../config/prisma')
 
 async function getAllProducts(req, res) {
   try {
     const products = await prisma.products.findMany()
-    res.json({ products: [...products], message: 'These are the products' })
+    res.status(200).json({ products: [...products], message: 'These are the products' })
   } catch (error) {
     res.status(500).json({ error: 'Failed to retrieve products. Please try again later.' })
   } finally {
