d30c6e7bfa034e9b0d247effe72c697b6d71cb6c
valenn0101 <veroncaceresvalentin@gmail.com>
2023-05-04 13:33:03 -0300

    To allow requests from localhost

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const cookieParser = require('cookie-parser')
 const routes = require('./v1/routes/crudRoutes')
 const logger = require('./config/logger')
 
-
 app
-app.use((err, req, res, next) => {
-  logger.error(err.stack)
-  res.status(500).send('Ha ocurrido un error.') 
-})
-  .use(cors())
+app
+  .use((err, req, res, next) => {
+    logger.error(err.stack)
+    res.status(500).send('Ha ocurrido un error.')
+  })
+  .use(cors({ origin: 'http://localhost:3000', credentials: true }))
   .use(cookieParser())
   .use(express.json())
   .use(bodyParser.json())
