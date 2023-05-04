b669568044cfceda279498cc4d1e4d8a5ebb3aea
valenn0101 <veroncaceresvalentin@gmail.com>
2023-05-04 15:27:28 -0300

    To allow all types of requests.

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const routes = require('./v1/routes/crudRoutes')
 const logger = require('./config/logger')
 
 app
-app
   .use((err, req, res, next) => {
     logger.error(err.stack)
     res.status(500).send('Ha ocurrido un error.')
@@ -17,6 +16,13 @@ app
   .use(express.json())
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: true }))
+  .use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Set-Cookie')
+    res.header('Access-Control-Allow-Credentials', 'true')
+    next()
+  })
   .use('/api/v1/crud', routes)
 
 module.exports = app
